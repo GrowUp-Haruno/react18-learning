fd25ac337fb0064f6fe2b5820b379c8aaf7b28b3
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-11-02 11:02:37 +0900

    Promiseでのバッチ処理 close #5

---
+++ src/components/AutomaticBatcing/AutoBatchPromise.tsx
@@ -0,0 +1,37 @@
+import { FC, useState } from 'react';
+import { flushSync } from 'react-dom';
+import { pokeDataType } from '../../commons/model/pokeApi';
+
+export const AutoBatchPromise: FC = () => {
+  console.log('AutoBatchPromise is rendering');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [pokemons, setPokemons] = useState<pokeDataType | null>(null);
+
+  // イベントハンドラとPromise内ではそれぞれで自動バッチが実行される
+  const pokemonGet = () => {
+    setIsLoading(true);
+    // 完了後、再レンダリングする
+    fetch('https://pokeapi.co/api/v2/pokemon')
+      .then((res) => res.json() as Promise<pokeDataType>)
+      .then((data) => {
+        setPokemons(data);
+        setIsLoading(false);
+        // 完了後、再レンダリングする
+      });
+  };
+
+  return (
+    <div>
+      <p>Automatic Batching（Promise）</p>
+      <p>ボタンを押すとデータを取得します</p>
+      <button onClick={pokemonGet}>ゲットだぜ！</button>
+      {isLoading ? (
+        <p>Now Loading...</p>
+      ) : (
+        pokemons?.results.map((result) => (
+          <p key={result.name}>{result.name}</p>
+        ))
+      )}
+    </div>
+  );
+};
