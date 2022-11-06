62a52cfdda75414ce766cdee4406e177130d398f
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-11-06 13:41:08 +0900

    汎用コンポーエントを作成

--- src/components/atoms/Layout.tsx
+++ src/components/atoms/Layout.tsx
@@ -12,3 +12,40 @@ export const ShowComponent: FC<ShowComponentProps> = ({ isOpen, children }) => {
     />
   );
 };
+
+type HStackProps = {
+  children: ReactNode;
+};
+export const HStack: FC<HStackProps> = (props) => (
+  <div
+    {...props}
+    style={{ display: 'flex', padding: '16px', width: '1000px' }}
+  />
+);
+
+type FlexGrowBoxProps = {
+  children: ReactNode;
+};
+export const FlexGrowBox: FC<FlexGrowBoxProps> = ({ ...props }) => (
+  <div
+    {...props}
+    style={{
+      flexGrow: 1,
+    }}
+  />
+);
+
+type BoxProps = {
+  children: ReactNode;
+  height?: string;
+};
+export const Box: FC<BoxProps> = ({ height, ...props }) => (
+  <div
+    {...props}
+    style={{
+      height: height,
+      overflowY: 'scroll',
+      border: '1px solid gray',
+    }}
+  />
+);
---
+++ src/components/atoms/TextMessage.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+
+export const LoadingMessage: FC = () => {
+  return <p>Now Loading...</p>;
+};
+export const ErrorMessage: FC = () => {
+  return <p>通信エラー</p>;
+};
